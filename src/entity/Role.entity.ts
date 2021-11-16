540aac798da31ee1456ba5ed4ddd755bc7d449de
sonhai32 <hailam32.dev@gmail.com>
2021-11-16 14:41:21 +0700

    Update role entity

--- src/entity/Role.entity.ts
+++ src/entity/Role.entity.ts
@@ -10,7 +10,7 @@ import {
 @Entity()
 export class Role {
   @PrimaryGeneratedColumn("increment")
-  id!: string;
+  id!: number;
 
   @Column({ type: "nvarchar", length: 32, default: "User" })
   name!: string;
@@ -20,5 +20,11 @@ export class Role {
     length: 100,
     nullable: true,
   })
-  description!: string;
+  description?: string;
+
+  constructor(id: number, name: string, description?: string) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+  }
 }
